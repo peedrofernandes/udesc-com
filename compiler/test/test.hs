3b6a3e83c4c36c4e91071195727dc6fa8830bfad
Pedro <gm.pedro.fernandes@gmail.com>
2023-07-10 20:25:13 -0300

    Adicionado ./compile como um submódulo

--- compiler/test/test.hs
+++
@@ -1,8 +0,0 @@
--- type Id = String
--- type Var = (Id, Tipo)
--- data Tipo = TDouble | TInt | TString | TVoid deriving Show
-
--- getVars :: [(Tipo, [Id])] -> [Var]
--- getVars [] = []
--- getVars ((tipo, []) : t) = getVars(t)
--- getVars ((tipo, id : ids) : t) = (id, tipo) : getVars ((tipo, ids) : t)
